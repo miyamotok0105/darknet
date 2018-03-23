e6abbf1e9a39a9ead7d3b64ca07d4a78ba65a727
Ubuntu <ubuntu@ip-172-31-54-107.ec2.internal>
2018-03-23 07:35:31 +0000

    update voc label

--- voc_label.py
+++ voc_label.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
-import os
-import cv2
-import glob
-import numpy as np
-from PIL import Image, ImageTk
 import xml.etree.ElementTree as ET
 import pickle
 import os
 from os import listdir, getcwd
 from os.path import join
 
-#元の奴box 0 1 2 3
-#xmin xmax ymin ymax
-#学習データセット作成
-#xmin ymin xmax ymax
+sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
 
 def convert(size, box):
     dw = 1./size[0]
@@ -28,24 +22,35 @@ def convert(size, box):
     h = h*dh
     return (x,y,w,h)
 
-for name in glob.glob("/home/hoge/labels/*.txt"):
-    for line in open(name, 'r'):
-    print("label name is " + name)
-    print("image name is " + name.replace("labels","images").replace(".txt",".jpg"))
-    img = cv2.imread(name.replace("labels","images").replace(".txt",".jpg"))
-        h, w = img.shape[:2]
-    print(line)
-    l = line.split(" ")
-        print l[0]
-    print l[1]
-    print l[2]
-    print l[3]
-    print l[4]
+def convert_annotation(year, image_id):
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
 
-    b = (float(l[1]), float(l[3]), float(l[2]), float(l[4]))
-    bb = convert((w,h), b)
-    print(str(l[0]) + " " + " ".join([str(a) for a in bb]))
-    list_write_file = open(name.replace("labels", "labels_"), 'a')
-    list_write_file.write(str(l[0]) + " " + " ".join([str(a) for a in bb]) + "\n")
-    list_write_file.close()
-    #break
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult) == 1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        bb = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+wd = getcwd()
+
+for year, image_set in sets:
+    if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):
+        os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    for image_id in image_ids:
+        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
+        convert_annotation(year, image_id)
+    list_file.close()
+
